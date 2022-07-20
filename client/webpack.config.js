c8e5acc9804d648a72060b7031c682686820d75b
raydover <105178034+raydover@users.noreply.github.com>
2022-07-20 14:32:17 -0400

    heroku create

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const {InjectManifest} = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = () => {
@@ -11,17 +14,45 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      // new InjectManifest({
+      //   swSrc: './src/sw.js',
+      //   swDest: 'service-worker.js',
+      // }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
   };
 };
